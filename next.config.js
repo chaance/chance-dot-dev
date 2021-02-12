af886a7b806ab48c1bf1853f242cbbb46be62d9b
Chance Strickland <hi@chancedigital.io>
2021-02-11 19:03:20 -0800

    add mdx top-level page support

--- next.config.js
+++ next.config.js
@@ -5,4 +5,25 @@ module.exports = {
 	},
 	//target: "serverless",
 	target: "experimental-serverless-trace",
+	pageExtensions: ["js", "jsx", "tsx", "mdx"],
+	webpack(config, options) {
+		config.module.rules.push({
+			test: /\.mdx$/,
+			use: [
+				options.defaultLoaders.babel,
+				{
+					loader: require.resolve("@mdx-js/loader"),
+					options: {
+						remarkPlugins: [
+							require("remark-images"),
+							require("remark-slug"),
+							require("@ngsctt/remark-smartypants"),
+						],
+						rehypePlugins: [],
+					},
+				},
+			],
+		});
+		return config;
+	},
 };
