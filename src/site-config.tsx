557ed65029f28e0fd98a56178e74e2afc7d3b415
Chance Strickland <hi@chancedigital.io>
2021-02-14 17:33:46 -0800

    rm unused config keys

--- src/site-config.tsx
+++ src/site-config.tsx
@@ -1,5 +1,14 @@
 import { NextSeoProps } from "next-seo";
 
+const TWITTER_USER_NAME = "chancethedev";
+const TWITTER_HANDLE = "@" + TWITTER_USER_NAME;
+const SITE_TITLE = "chance.dev";
+const SITE_DESC = "Musings on things that interest me";
+const SITE_URL =
+	process.env.NODE_ENV === "development"
+		? "http://localhost:3000"
+		: "https://chance.dev";
+
 export const authors: Author[] = [
 	{
 		id: "chance",
@@ -10,127 +19,65 @@ export const authors: Author[] = [
 	},
 ];
 
-const config: Config = (function ({
-	twitterUserName,
-	siteTitle,
-	siteUrl,
-}: {
-	twitterUserName: string;
-	siteTitle: string;
-	siteUrl: string;
-}) {
-	const twitterHandle = `@${twitterUserName}`;
-	return {
-		pathPrefix: "/",
-		siteTitle,
-		siteTitleAlt: "Musings on things that interest me",
-		siteTitleShort: siteTitle,
-		siteUrl,
-		siteLanguage: "en",
-		siteDescription: "Musings on things that interest me",
-		author: authors.find((author) => author.id === "chance") || authors[0],
-		organization: "Chance Digital",
-		keywords: ["web development", "JavaScript", "React", "politics"],
-		twitterUserName,
-		typekitId: "isw6kyj",
+const config: Config = {
+	pathPrefix: "/",
+	siteTitle: SITE_TITLE,
+	siteUrl: SITE_URL,
 
-		// Manifest and Progress color
-		themeColor: "#e51574",
-		backgroundColor: "#222223",
-
-		// Social component
-		github: "https://github.com/chancestrickland/",
-		stackOverflow: "https://stackoverflow.com/users/1792019/chance-strickland",
-		linkedin: "https://www.linkedin.com/in/chancestrickland/",
-		userTwitter: `@${twitterUserName}`,
-		twitter: `https://twitter.com/${twitterUserName}/`,
-		twitterHandle,
-
-		// SEO
-		seo: {
-			title: siteTitle,
-			titleTemplate: "%s | chance.dev",
-			description: "Musings on things that interest me",
-			openGraph: {
-				type: "website",
-				locale: "en_US",
-				url: siteUrl,
-				site_name: siteTitle,
-				images: [
-					{
-						url:
-							"https://res.cloudinary.com/chancedigital/image/upload/v1612287816/cs.run/og-image.jpg",
-						alt: "Welcome to chance.dev!",
-						width: 1200,
-						height: 630,
-					},
-					// {
-					// 	url: 'https://www.example.ie/og-image.jpg',
-					// 	width: 800,
-					// 	height: 600,
-					// 	alt: 'Og Image Alt',
-					// },
-					// {
-					// 	url: 'https://www.example.ie/og-image-2.jpg',
-					// 	width: 800,
-					// 	height: 600,
-					// 	alt: 'Og Image Alt 2',
-					// },
-				],
-			},
-			twitter: {
-				handle: twitterHandle,
-				site: twitterHandle,
-				cardType: "summary_large_image",
-			},
+	// SEO
+	seo: {
+		title: SITE_TITLE,
+		titleTemplate: "%s | chance.dev",
+		description: SITE_DESC,
+		openGraph: {
+			type: "website",
+			locale: "en_US",
+			url: SITE_URL,
+			site_name: SITE_TITLE,
+			images: [
+				{
+					url:
+						"https://res.cloudinary.com/chancedigital/image/upload/v1612287816/cs.run/og-image.jpg",
+					alt: "Welcome to chance.dev!",
+					width: 1200,
+					height: 630,
+				},
+				// {
+				// 	url: 'https://www.example.ie/og-image.jpg',
+				// 	width: 800,
+				// 	height: 600,
+				// 	alt: 'Og Image Alt',
+				// },
+				// {
+				// 	url: 'https://www.example.ie/og-image-2.jpg',
+				// 	width: 800,
+				// 	height: 600,
+				// 	alt: 'Og Image Alt 2',
+				// },
+			],
 		},
-	};
-})({
-	twitterUserName: "chancethedev",
-	siteTitle: "chance.dev",
-	siteUrl:
-		process.env.NODE_ENV === "development"
-			? "http://localhost:3000"
-			: "https://chance.dev",
-});
+		twitter: {
+			handle: TWITTER_HANDLE,
+			site: TWITTER_HANDLE,
+			cardType: "summary_large_image",
+		},
+	},
+};
 
 type Config = {
 	pathPrefix: string;
 	siteTitle: string;
-	siteTitleAlt: string;
-	siteTitleShort: string;
 	siteUrl: string;
-	siteLanguage: string;
-	siteDescription: string;
-	author: Author;
-	organization: string;
-	keywords: string[];
-	twitterUserName: string;
-	typekitId: string;
-
-	// Manifest and Progress color
-	themeColor: string;
-	backgroundColor: string;
-
-	// Social component
-	github: string;
-	stackOverflow: string;
-	linkedin: string;
-	userTwitter: string;
-	twitter: string;
-	twitterHandle: string;
-
-	// SEO
 	seo: NextSeoProps;
 };
 
 export { config };
 export default config;
 
-type Author = {
+interface Author {
 	name: string;
 	image: string;
 	id: string;
 	email: string;
 	bio?: string;
-};
+}
