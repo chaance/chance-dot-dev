7d9c8ffee3fc334c929a60cdaefd1593fd375589
Chance Strickland <hi@chance.dev>
2022-11-28 19:53:26 -0800

    update e2e test

--- cypress/e2e/smoke.cy.ts
+++ cypress/e2e/smoke.cy.ts
@@ -1,29 +1,37 @@
-import { faker } from "@faker-js/faker";
+// import { faker } from "@faker-js/faker";
 
 describe("smoke tests", () => {
 	afterEach(() => {
 		cy.cleanupUser();
 	});
 
-	it("should allow you to register and login", () => {
+	// it("should allow you to register", () => {
+	// 	const loginForm = {
+	// 		email: `${faker.internet.userName()}@example.com`,
+	// 		password: faker.internet.password(),
+	// 	};
+
+	// 	cy.then(() => ({ email: loginForm.email })).as("user");
+
+	// 	cy.visitAndCheck("/join");
+
+	// 	cy.findByRole("textbox", { name: /email/i }).type(loginForm.email);
+	// 	cy.findByLabelText(/password/i).type(loginForm.password);
+	// 	cy.findByRole("button", { name: /create account/i }).click();
+	// });
+
+	it("should allow you to login", () => {
 		const loginForm = {
-			email: `${faker.internet.userName()}@example.com`,
-			password: faker.internet.password(),
+			email: process.env.AUTHENTICATED_EMAIL!,
+			password: process.env.AUTHENTICATED_EMAIL_PASSWORD!,
 		};
 
 		cy.then(() => ({ email: loginForm.email })).as("user");
 
-		cy.visitAndCheck("/");
-
-		cy.findByRole("link", { name: /sign up/i }).click();
-
+		cy.visitAndCheck("/login");
 		cy.findByRole("textbox", { name: /email/i }).type(loginForm.email);
 		cy.findByLabelText(/password/i).type(loginForm.password);
-		cy.findByRole("button", { name: /create account/i }).click();
-
-		cy.findByRole("link", { name: /notes/i }).click();
-		cy.findByRole("button", { name: /logout/i }).click();
-		cy.findByRole("link", { name: /log in/i });
+		cy.findByRole("button", { name: /log in/i }).click();
 	});
 
 	// it("should allow you to make a note", () => {
