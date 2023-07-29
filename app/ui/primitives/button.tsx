37a1c35147ecf23365a385ef4ac8cfd0dc9d607a
Chance Strickland <hi@chance.dev>
2023-07-29 12:20:40 -0700

    fix button props

--- app/ui/primitives/button.tsx
+++ app/ui/primitives/button.tsx
@@ -1,20 +1,54 @@
 import * as React from "react";
-import { useButton } from "@react-aria/button";
+import { type AriaButtonProps, useButton } from "@react-aria/button";
 import type { PressEvents, FocusableProps } from "~/types/react-aria";
+
+function useButtonWrapped<Elem extends HTMLDivElement | HTMLButtonElement>(
+	props: Omit<
+		AriaButtonProps<Elem extends HTMLButtonElement ? "button" : "div">,
+		keyof PressEvents | keyof FocusableProps
+	> &
+		PressEvents<Elem> &
+		FocusableProps<Elem>,
+	ref: React.RefObject<Elem>
+) {
+	const {
+		autoFocus,
+		elementType,
+		excludeFromTabOrder,
+		isDisabled,
+		onBlur,
+		onFocus,
+		onFocusChange,
+		onPress,
+		onPressChange,
+		onPressEnd,
+		onPressStart,
+		onPressUp,
+		...domProps
+	} = props;
+	let { buttonProps, isPressed } = useButton(props as any, ref);
+	return {
+		buttonProps: {
+			...domProps,
+			...buttonProps,
+		},
+		isPressed,
+	};
+}
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
 	({ children, ...props }, forwardedRef) => {
 		let ref = React.useRef<HTMLButtonElement>(null);
-		let { buttonProps } = useButton(
+		let { buttonProps } = useButtonWrapped(
 			{
-				...(props as any),
+				...props,
 				excludeFromTabOrder: props.tabIndex != null && props.tabIndex !== 0,
 				isDisabled: props.disabled,
 			},
 			ref
 		);
 		return (
-			<button ref={forwardedRef} {...props} {...buttonProps}>
+			<button ref={forwardedRef} {...buttonProps}>
 				{children}
 			</button>
 		);
@@ -25,7 +59,7 @@ Button.displayName = "Button";
 const ButtonDiv = React.forwardRef<HTMLDivElement, ButtonDivProps>(
 	({ children, ...props }, forwardedRef) => {
 		let ref = React.useRef<HTMLDivElement>(null);
-		let { buttonProps } = useButton(
+		let { buttonProps } = useButtonWrapped(
 			{
 				...(props as any),
 				elementType: "div",
@@ -35,7 +69,7 @@ const ButtonDiv = React.forwardRef<HTMLDivElement, ButtonDivProps>(
 			ref
 		);
 		return (
-			<div ref={forwardedRef} {...props} {...buttonProps}>
+			<div ref={forwardedRef} {...buttonProps}>
 				{children}
 			</div>
 		);
@@ -49,7 +83,10 @@ interface ButtonOwnProps
 
 interface ButtonProps
 	extends ButtonOwnProps,
-		Omit<React.ComponentPropsWithRef<"button">, keyof ButtonOwnProps> {}
+		Omit<
+			React.ComponentPropsWithRef<"button">,
+			keyof ButtonOwnProps | "onClick"
+		> {}
 
 interface ButtonDivOwnProps
 	extends PressEvents<HTMLDivElement>,
@@ -62,4 +99,4 @@ interface ButtonDivProps
 		Omit<React.ComponentPropsWithRef<"div">, keyof ButtonDivOwnProps> {}
 
 export type { ButtonProps, ButtonDivProps };
-export { Button, ButtonDiv };
+export { Button, ButtonDiv, useButtonWrapped as useButton };
