d8df1bc6ff64c9a1d21597243666cd173e69eabf
Chance Strickland <hi@chance.dev>
2023-07-29 06:59:53 -0700

    eslint bs

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,6 +21,7 @@ const config = {
 		"prefer-const": "off",
 		"@typescript-eslint/ban-ts-comment": "off",
 		"@typescript-eslint/no-non-null-assertion": "off",
+		"@typescript-eslint/no-non-null-asserted-optional-chain": "off",
 		"@typescript-eslint/no-empty-interface": "off",
 		"@typescript-eslint/no-unused-vars": "warn",
 	},
