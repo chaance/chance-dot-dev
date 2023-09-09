ffb4f344fcee5bc4ddaf6697ccf7952c8d32c983
Chance Strickland <hi@chance.dev>
2023-09-08 17:13:15 -0700

    use ~ paths for css imports

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,3 +1,5 @@
+import path from "node:path";
+import url from "node:url";
 import postcssGlobalData from "@csstools/postcss-global-data";
 import postcssImport from "postcss-import";
 import postcssCustomMedia from "postcss-custom-media";
@@ -5,20 +7,27 @@ import autoprefixer from "autoprefixer";
 import cssnano from "cssnano";
 import postcssNesting from "postcss-nesting";
 
+const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
+
 const config = (ctx) => {
 	const isProd = ctx.env === "production";
 	return {
 		plugins: [
+			postcssImport({
+				resolve: (id) => {
+					// resolve `~/` prefixed imports to the `app` directory
+					if (id.startsWith("~/")) {
+						return path.resolve(__dirname, `app/${id.slice(2)}`);
+					}
+					return id;
+				},
+			}),
 			postcssGlobalData({
 				files: ["app/styles/system/media.css"],
 			}),
-			postcssImport(),
 			postcssNesting(),
 			postcssCustomMedia(),
-			autoprefixer({
-				flexbox: false,
-				grid: false,
-			}),
+			autoprefixer({ flexbox: false, grid: false }),
 			isProd && cssnano({ preset: "default" }),
 		].filter(Boolean),
 	};
