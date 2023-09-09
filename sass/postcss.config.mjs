0280068563a79f52a39e5b2d3708b350bedaae92
Chance Strickland <hi@chance.dev>
2023-09-08 17:00:36 -0700

    use built-in postcss support

--- sass/postcss.config.mjs
+++ sass/postcss.config.mjs
@@ -1,19 +1,30 @@
+import path from "node:path";
 import postcssGlobalData from "@csstools/postcss-global-data";
+import nodeSassAliasImporter from "node-sass-alias-importer";
 import postcssImport from "postcss-import";
 import postcssCustomMedia from "postcss-custom-media";
+import { postcssSass } from "../scripts/postcss-sass.mjs";
 import autoprefixer from "autoprefixer";
 import cssnano from "cssnano";
 import postcssNesting from "postcss-nesting";
 
 const config = (ctx) => {
+	const isScss = ctx.file.extname === ".scss";
 	const isProd = ctx.env === "production";
+
 	return {
+		parser: isScss ? "postcss-scss" : false,
+		syntax: isScss ? "postcss-scss" : false,
 		plugins: [
 			postcssGlobalData({
-				files: ["app/styles/system/media.css"],
+				files: [path.resolve(process.cwd(), "app/styles/system", "media.css")],
 			}),
-			postcssImport(),
-			postcssNesting(),
+			isScss &&
+				postcssSass({
+					importer: nodeSassAliasImporter({ "~": "." }, { root: "./app" }),
+				}),
+			!isScss && postcssImport(),
+			!isScss && postcssNesting(),
 			postcssCustomMedia(),
 			autoprefixer({
 				flexbox: false,
