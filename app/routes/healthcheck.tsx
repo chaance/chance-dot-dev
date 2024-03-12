d1d78e4d5b91174a62748698158c834896991242
Chance Strickland <hi@chance.dev>
2024-03-12 13:08:24 -0700

    update health check query

--- app/routes/healthcheck.tsx
+++ app/routes/healthcheck.tsx
@@ -11,7 +11,7 @@ export async function loader({ request }: LoaderFunctionArgs) {
 		// if we can connect to the database and make a simple query
 		// and make a HEAD request to ourselves, then we're good.
 		await Promise.all([
-			prisma.user.count(),
+			prisma.blogPost.count(),
 			fetch(url.toString(), { method: "HEAD" }).then((r) => {
 				if (!r.ok) return Promise.reject(r);
 			}),
