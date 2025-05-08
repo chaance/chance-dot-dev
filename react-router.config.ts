8035d562ec37e618dca96b922eafa7711611cef0
Chance Strickland <hi@chance.dev>
2025-05-08 15:19:47 -0400

    use RR future flags

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,3 +1,10 @@
 import type { Config } from "@react-router/dev/config";
 
-export default { ssr: true } satisfies Config;
+export default {
+	ssr: true,
+	future: {
+		unstable_optimizeDeps: true,
+		unstable_splitRouteModules: "enforce",
+		unstable_middleware: true,
+	},
+} satisfies Config;
