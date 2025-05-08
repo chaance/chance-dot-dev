6e039ac0aa02bf247b0fd8bfb6cec91b3fd8dda0
Chance Strickland <hi@chance.dev>
2025-05-08 15:41:56 -0400

    server refactor

--- server.mjs
+++ server.mjs
@@ -1,31 +1,21 @@
+// @ts-check
 import * as path from "node:path";
 import * as fs from "node:fs";
-import * as url from "node:url";
+import * as util from "node:util";
 import express from "express";
 import compression from "compression";
 import morgan from "morgan";
-import { createRequestHandler } from "@react-router/express";
 import rateLimit from "express-rate-limit";
 import sourceMapSupport from "source-map-support";
 import { createStream } from "rotating-file-stream";
 import { getRedirectsMiddleware } from "./server/redirects.js";
+import getPort from "get-port";
 
-const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
-
-const MODE = process.env.NODE_ENV;
-const STAGING = process.env.IS_STAGING === "true";
-const IS_DEV = MODE === "development";
-const IS_PROD = MODE === "production" && !STAGING;
+const IS_STAGING = process.env.IS_STAGING === "true";
+const IS_DEV = process.env.NODE_ENV === "development";
+const IS_PROD = process.env.NODE_ENV === "production" && !IS_STAGING;
 
 sourceMapSupport.install();
-
-const viteDevServer = IS_DEV
-	? await import("vite").then((vite) =>
-			vite.createServer({
-				server: { middlewareMode: true },
-			}),
-		)
-	: undefined;
 
 const app = express();
 
@@ -40,10 +30,10 @@ const limiter = rateLimit({
 app.use(limiter);
 
 app.all(
-	"*",
+	"*splat",
 	getRedirectsMiddleware({
 		redirectsString: fs.readFileSync(
-			path.join(__dirname, "server/_redirects.txt"),
+			path.join(import.meta.dirname, "server/_redirects.txt"),
 			"utf8",
 		),
 	}),
@@ -64,26 +54,61 @@ app.use(compression());
 // http://expressjs.com/en/advanced/best-practice-security.html#at-a-minimum-disable-x-powered-by-header
 app.disable("x-powered-by");
 
-if (viteDevServer) {
+if (IS_DEV) {
+	console.log(util.styleText("green", "Starting development server 🚀"));
+	const viteDevServer = await import("vite").then((vite) =>
+		vite.createServer({
+			server: {
+				middlewareMode: true,
+			},
+		}),
+	);
 	app.use(viteDevServer.middlewares);
+	app.use(async (req, res, next) => {
+		try {
+			const source = await viteDevServer.ssrLoadModule("./server/index.ts");
+			return await source.app(req, res, next);
+		} catch (error) {
+			if (error != null && typeof error === "object" && "message" in error) {
+				try {
+					// @ts-expect-error
+					viteDevServer.ssrFixStacktrace(error);
+				} catch {}
+			}
+			next(error);
+		}
+	});
 } else {
+	console.log(util.styleText("green", "Starting production server"));
 	// Remix fingerprints its assets so we can cache forever.
 	app.use(
 		"/assets",
 		express.static("build/client/assets", { immutable: true, maxAge: "1y" }),
 	);
+	app.use(
+		// browser 1 hour, server 1 year
+		express.static("build/client", {
+			setHeaders: (res) => {
+				res.setHeader(
+					"Cache-Control",
+					"public, max-age=3600, s-maxage=31536000",
+				);
+			},
+		}),
+	);
+
+	app.use(
+		// this may or may not exist depending on the state of the build
+		// @ts-ignore
+		await import("./build/server/index.js").then((mod) => mod.app),
+	);
 }
 
-// Everything else (like favicon.ico) is cached for an hour. You may want to be
-// more aggressive with this caching.
-app.use(express.static("build/client", { maxAge: "1h" }));
-
-/** @type {import("rotating-file-stream").RotatingFileStream | undefined} */
 let accessLogStream;
 if (IS_PROD) {
 	accessLogStream = createStream("access.log", {
 		interval: "1d",
-		path: path.join(__dirname, "log"),
+		path: path.join(import.meta.dirname, "log"),
 	});
 }
 
@@ -94,26 +119,22 @@ app.use(
 	}),
 );
 
-app.all(
-	"*",
-	createRequestHandler({
-		build: viteDevServer
-			? () => viteDevServer.ssrLoadModule("virtual:react-router/server-build")
-			: // this may or may not exist depending on the state of the build
-				// @ts-ignore
-				await import("./build/server/index.js"),
-		mode: process.env.NODE_ENV,
-	}),
-);
-const port = process.env.PORT || 3000;
+let port;
+if (process.env.PORT) {
+	port = process.env.PORT;
+} else if (IS_DEV) {
+	port = await getPort({
+		port: Array.from({ length: 10 }, (_, i) => 3000 + i),
+	});
+} else {
+	port = 8080;
+}
 
 app.listen(port, () => {
 	console.log(
-		`Express server listening on port ${port} (http://localhost:${port})`,
+		util.styleText(
+			"blue",
+			`Express server listening on port ${port} (http://localhost:${port})`,
+		),
 	);
 });
-
-/**
- * @typedef {import("path-to-regexp").Key} Key
- * @typedef {import("express").RequestHandler} RequestHandler
- */
